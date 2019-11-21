3d300bc912b7b8cce9ee27a65d056795dd2d2c9c
lelani82 <lelaniparker@hotmail.com>
2019-11-21 11:11:29 +1000

    added environmental variable

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // Create an axios instance
 export default axios.create({
-  baseURL: 'http://localhost:3009',
-  timeout: 2000,
+  //baseURL: 'http://localhost:3009',
+  baseURL: process.env.EXPRESS_URI || 'https://limitless-lowlands-34451.herokuapp.com/',
+  timeout: 5000,
   withCredentials: true
 })
